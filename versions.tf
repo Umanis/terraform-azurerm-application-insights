4f43b86054c0212d464871ba0df8c1c696e887c4
AzureDevOps agent <vsts@fv-az234-103.xfwla30uekyuppkxcmnnupgqeg.ax.internal.cloudapp.net>
2021-08-24 19:15:14 +0000

    Artefact release 1.0.0

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.71.0"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = ">= 1.2.5"
+    }
+  }
+}
